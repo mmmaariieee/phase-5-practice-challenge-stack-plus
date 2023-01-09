a67c09f8a213f56cce75567a2f5e581a33013c1c
Mariia <m.goncharenkoo@icloud.com>
2023-01-09 12:51:41 -0500

    passed tests

--- lib/stack_plus.rb
+++ lib/stack_plus.rb
@@ -1,17 +1,50 @@
+# class StackPlus
+#   attr_reader :data
+
+#   def initialize
+#     @data = []
+#   end
+
+#   def push(value)
+#     data.push(value)
+#   end
+
+#   def pop
+#     return -1 unless data.size > 0
+#     data.pop
+#   end
+
+#   def increment(n)
+
+#     [n, data.size].min.times do |i|
+#       data[i] += 1
+#     end
+#   end
+# end
+
+
 class StackPlus
+  attr_reader :inc, :data
+
   def initialize
-    # your code here
+    @inc = []
+    @data = []
   end
 
   def push(value)
-    # your code here
+    data.push(value)
+    inc.push(0)
   end
 
   def pop
-    # your code here
+    return -1 unless data.size > 0
+    inc_val = inc.pop
+    inc[inc.size - 1] += inc_val if inc.size > 0
+    data.pop + inc_val
   end
 
   def increment(n)
-    # your code here
+    i = [n, inc.size].min - 1
+    inc[i] += 1 if inc[i]
   end
 end
